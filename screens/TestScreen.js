675f9b2158f6dde8042992e912e996b254ac0782
Hakim Laoukili <hakimlaoukili@gmail.com>
2021-02-11 22:44:13 +0100

    Creat TestScreen
    Created a new test screen for new funtions

---
+++ screens/TestScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const TestScreen = () => {
+  return (
+    <View>
+      <Text> Hola !!</Text>
+    </View>
+  );
+};
+
+export default TestScreen;
+
+const styles = StyleSheet.create({});
